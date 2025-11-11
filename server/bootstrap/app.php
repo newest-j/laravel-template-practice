6728f550be4d0cf16c5c8ad4e8e472da8465a71d
newest-j <ibukunagbaoye@gmail.com>
2025-11-11 03:13:31 +0100

    hasapitoken added to user model and comment to app/php

--- server/bootstrap/app.php
+++ server/bootstrap/app.php
@@ -7,6 +7,9 @@ use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
+        // php artisan install:api
+        // this is this to install the route.api and the auth:sanctumn in the auth.php
+
         web: __DIR__ . '/../routes/web.php',
         api: __DIR__ . '/../routes/api.php',
         commands: __DIR__ . '/../routes/console.php',
@@ -15,6 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         // Web group (use Illuminate middleware, not App\ stubs)
         $middleware->web([
+            // in laravel 12 we do not define our middleware in the kernel.php but here in the bootstap/app.php
             \Illuminate\Cookie\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
